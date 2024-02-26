6af396efebb14f1b2dd70acf1ee1d0348aa1dd5c
akharearti <aratiakhare123@gmail.com>
2024-02-27 03:08:41 +0530

    Complete PF

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent the default form submission
+    // You can add your logic here for form submission, e.g., sending data to a server
+    alert('Form submitted successfully!');
+  });
+  
